98b4af6f9ad77fcc6f055aabe3b209341a5f4421
Carl X. Su <bcbcarl@gmail.com>
2015-09-28 17:37:46 +0800

    Disable pyenv, rbenv and Android SDK by default

--- config.fish
+++ config.fish
@@ -1,7 +1,14 @@
 set fish_path "$HOME/.config/fish"
 set -x PATH "/usr/local/sbin" $PATH
 
+# Git
 . $fish_path/git.fish
-. $fish_path/pyenv.fish
-. $fish_path/rbenv.fish
-. $fish_path/android.fish
+
+# pyenv
+#. $fish_path/pyenv.fish
+
+# rbenv
+#. $fish_path/rbenv.fish
+
+# Android SDK
+#. $fish_path/android.fish
