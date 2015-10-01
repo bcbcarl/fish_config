063de3205230791cbcda58736ffe4a46202a0827
Carl X. Su <bcbcarl@gmail.com>
2015-10-01 15:39:36 +0800

    Fix Android config

--- android.fish
+++ android.fish
@@ -1,4 +1,4 @@
-set JAVA_HOME (/usr/libexec/java_home)
-set ANDROID_HOME "$HOME/Library/Android/sdk"
+set -x JAVA_HOME (/usr/libexec/java_home)
+set -x ANDROID_HOME "$HOME/Library/Android/sdk"
 alias adb "$ANDROID_HOME/platform-tools/adb"
 alias emulator "$ANDROID_HOME/tools/emulator"
