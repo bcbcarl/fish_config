8a99f46f649c418cf93aa83628e00da98548e7a5
Carl X. Su <bcbcarl@gmail.com>
2016-03-05 17:20:13 +0800

    Add git push origin <current_branch> alias

--- git.fish
+++ git.fish
@@ -1,3 +1,7 @@
+function _git_current_branch --description 'Show Git current branch'
+  git name-rev --name-only HEAD
+end
+
 function _git_diff --description 'Fancy Git diffs'
   git diff --color $argv | diff-so-fancy | less --tabs=4 -RFX
 end
@@ -10,6 +14,7 @@ alias gcb 'git checkout -b'
 alias gd '_git_diff'
 alias gdca '_git_diff --cached'
 alias gdf 'git diff'
+alias ggpush 'git push origin (_git_current_branch)'
 alias glgga 'git log --graph --decorate --all'
 alias gm 'git merge'
 alias gp 'git push'
