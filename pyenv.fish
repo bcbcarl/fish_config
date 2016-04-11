070ddac17c586f6d93a9a17a7947dc017d9f382d
Carl X. Su <bcbcarl@gmail.com>
2016-04-11 15:15:42 +0800

    Update pyenv config

--- pyenv.fish
+++ pyenv.fish
@@ -1,3 +1,17 @@
-set -x PATH "$HOME/.pyenv/bin" $PATH
+set -l pyenv_root ''
+
+if test -z "$PYENV_ROOT"
+  set pyenv_root "$HOME/.pyenv"
+  set -x PYENV_ROOT "$HOME/.pyenv"
+else
+  set pyenv_root "$PYENV_ROOT"
+end
+
+set -x PATH $pyenv_root/bin $PATH
+
+if not command -s pyenv > /dev/null
+  exit 1
+end
+
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
