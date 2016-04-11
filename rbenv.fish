fd65df4bd2a29cf661442aa9a65b3c595c13acff
Carl X. Su <bcbcarl@gmail.com>
2016-04-11 15:22:41 +0800

    Update rbenv config

--- rbenv.fish
+++ rbenv.fish
@@ -1,2 +1,16 @@
-set -x PATH "$HOME/.rbenv/bin" $PATH
+set -l rbenv_root ''
+
+if test -z "$RBENV_ROOT"
+  set rbenv_root "$HOME/.rbenv"
+  set -x RBENV_ROOT "$HOME/.rbenv"
+else
+  set rbenv_root "$RBENV_ROOT"
+end
+
+set -x PATH $rbenv_root/bin $PATH
+
+if not command -s rbenv > /dev/null
+  exit 1
+end
+
 status --is-interactive; and . (rbenv init -|psub)
