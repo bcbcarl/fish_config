6353c736617a5b8c1d41b0c8543a6e5567f56929
Carl X. Su <bcbcarl@gmail.com>
2016-04-12 00:24:41 +0800

    Add Docker config

---
+++ plugins/docker.fish
@@ -0,0 +1,25 @@
+# docker-machine
+alias dmstart 'docker-machine start'
+alias dmrestart 'docker-machine restart'
+alias dmstop 'docker-machine stop'
+alias dmstatus 'docker-machine status'
+alias dmenv 'docker-machine env --shell fish'
+alias dmip 'docker-machine ip'
+
+# docker
+abbr -a d docker
+alias dver 'docker version'
+alias dimg 'docker images'
+alias dbuild 'docker build'
+alias drm 'docker rm'
+alias drmi 'docker rmi'
+alias datt 'docker attach'
+alias dstart 'docker start'
+alias dstop 'docker stop'
+alias drun 'docker run'
+alias dsearch 'docker search'
+alias dpush 'docker push'
+alias dps 'docker ps'
+alias dpsa 'docker ps -a'
+
+eval (dmenv)
