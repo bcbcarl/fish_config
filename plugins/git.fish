4a48ad11b1c4539468716a7b617bf15c9eddd05d
Carl X. Su <bcbcarl@gmail.com>
2016-04-11 17:56:58 +0800

    Update git aliases to abbreviations

--- plugins/git.fish
+++ plugins/git.fish
@@ -1,27 +1,27 @@
 set -l git_diff '__git_diff'
 set -l git_current_branch '__git_current_branch'
 
-alias ga 'git add'
-alias gb 'git branch'
-alias gba 'git branch --all'
-alias gcb 'git checkout -b'
-alias gcm 'git checkout master'
-alias gcmsg 'git commit -m'
-alias gd "$git_diff"
-alias gdca "$git_diff --cached"
-alias gdf 'git diff'
-alias gf 'git fetch'
-alias gfa 'git fetch --all --prune'
-alias ggpush "git push origin $git_current_branch"
-alias glgga 'git log --graph --decorate --all'
-alias gm 'git merge'
-alias gp 'git push'
-alias gr 'git remote'
-alias gra 'git remote add'
-alias grup 'git remote update'
-alias grbi 'git rebase -i'
-alias gsb 'git status -sb'
-alias gst 'git status'
-alias gsta 'git stash'
-alias gstp 'git stash pop'
-alias gup 'git pull --rebase'
+abbr -a ga git add
+abbr -a gb git branch
+abbr -a gba git branch --all
+abbr -a gcb git checkout -b
+abbr -a gcm git checkout master
+abbr -a gcmsg git commit -m
+abbr -a gd $git_diff
+abbr -a gdca $git_diff --cached
+abbr -a gdf git diff
+abbr -a gf git fetch
+abbr -a gfa git fetch --all --prune
+abbr -a ggpush git push origin $git_current_branch
+abbr -a glgga git log --graph --decorate --all
+abbr -a gm git merge
+abbr -a gp git push
+abbr -a gr git remote
+abbr -a gra git remote add
+abbr -a grup git remote update
+abbr -a grbi git rebase -i
+abbr -a gsb git status -sb
+abbr -a gst git status
+abbr -a gsta git stash
+abbr -a gstp git stash pop
+abbr -a gup git pull --rebase
